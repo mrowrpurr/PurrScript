8fcb5f0bff11c018872a601cf1407749ce74ecb7
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-05 12:15:51 -0800

    xmake

--- xmake.lua
+++ xmake.lua
@@ -2,15 +2,6 @@ set_languages("c++20")
 
 add_rules("mode.debug", "mode.releasedbg")
 
-add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
 add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 
-includes("Papyrus.lua")
-includes("SkyScripts.lua")
-includes("Papyrus/*/xmake.lua")
-includes("SkyrimScripting/*/xmake.lua")
-includes("SkyrimScripting/*/*/xmake.lua")
-includes("PurrScript/*/xmake.lua")
-includes("SkyScript/*/xmake.lua")
-includes("Resources/*/xmake.lua")
-includes("Examples/*/xmake.lua")
+includes("*/xmake.lua")
