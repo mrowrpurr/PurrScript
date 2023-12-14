29fdb1146ae138295cb021d590d828f8767e7f96
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 19:33:14 -0800

    mode.release

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 set_languages("c++20")
 
-add_rules("mode.debug", "mode.releasedbg")
+add_rules("mode.debug", "mode.releasedbg", "mode.release")
 
 add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 
